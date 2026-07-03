5102cec9739a5423a1e5d53e7a96bc3f4ae84d1b
Carlos <carlos@pythoncheatsheet.org>
2026-07-03 15:14:11 -0400

    chore: add auto-update workflow with shasum markers

--- Formula/noodle.rb
+++ Formula/noodle.rb
@@ -7,19 +7,19 @@ class Noodle < Formula
   on_macos do
     on_arm do
       url "https://github.com/wilfredinni/noodle/releases/download/v0.1.0/noodle-macos-arm64"
-      sha256 "1fed92c472321ab5f1bcedb69f12ac470de5bb6538664bdc1af33c39ebcd10f8"
+      sha256 "1fed92c472321ab5f1bcedb69f12ac470de5bb6538664bdc1af33c39ebcd10f8" # macos-arm64
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/wilfredinni/noodle/releases/download/v0.1.0/noodle-linux-arm64"
-      sha256 "85a846e53afb9f26f541c9be2c190b06a5e44fb4991cd56d84fc7ed14090269b"
+      sha256 "85a846e53afb9f26f541c9be2c190b06a5e44fb4991cd56d84fc7ed14090269b" # linux-arm64
     end
 
     on_intel do
       url "https://github.com/wilfredinni/noodle/releases/download/v0.1.0/noodle-linux-x86_64"
-      sha256 "de653b6668c3585ab77d115834cb1db32adeafe73d5b44ab39f993789442eeb2"
+      sha256 "de653b6668c3585ab77d115834cb1db32adeafe73d5b44ab39f993789442eeb2" # linux-x86_64
     end
   end
 
